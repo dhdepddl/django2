5fd095d501136c827c4ba41f508f3feb28a65057
Young-eunKim <dhdepddl@gmail.com>
2017-06-01 19:09:33 +0900

    firebase test

--- recommend/pickme/classes/db.py
+++ recommend/pickme/classes/db.py
@@ -1,18 +1,17 @@
-from firebase import FirebaseAuthentication, FirebaseApplication
+from firebase import FirebaseAuthentication, FirebaseApplication, firebase
 from User import User
-import firebase_client
 
 
 class Firebase:
     def __init__(self):
         auth = FirebaseAuthentication('kUFM5wAt2CkXtfKrglMjLPgNsuWsO33j1uKHMRyn', 'dhdepddl@gmail.com', True, True)
-        self.db = firebase_client.Firebase('https://pickme-f283e.firebaseio.com/', auth)
+        self.db = firebase.FirebaseApplication('https://pickme-f283e.firebaseio.com/', auth)
 
     def get_initial_users(self, user_list):
-        users = list(self.db.child('/user-profiles/data').get())
+        users = list(self.db.get('/user-profiles/data', None, {'print': 'pretty'}))
         for user in users:
             user_list.append(User(user))
 
     def get_initial_posts(self):
-        posts = self.db.child('cards/data').get()
+        posts = self.db.get('cards/data', None)
         return posts
--- recommend/pickme/views.py
+++ recommend/pickme/views.py
@@ -2,5 +2,12 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render
+from django.http import JsonResponse
+import json
+from classes.db import Firebase
 
 # Create your views here.
+def space(request):
+    fb = Firebase()
+    posts = fb.get_initial_posts()
+    return JsonResponse(posts)
